471d1d8113e84d26c7c414e64a05affcae8c7aeb
masayuki <masayuki0214bg@gmail.com>
2020-10-10 03:52:17 +0900

    main

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(function() {
-  $('.shiba').hide();
+
+  $(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 200){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
 });
